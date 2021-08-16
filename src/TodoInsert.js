b3c3b6a3a0890cce55457dbb25b96f03d3c734f6
ainochi-kor <kthrkdals@gmail.com>
2021-08-16 23:43:13 +0900

    add

--- src/TodoInsert.js
+++ src/TodoInsert.js
@@ -11,11 +11,12 @@ const TodoInsert = ({ onInsert }) => {
 
   const onSubmit = useCallback(
     e => {
-      onInsert(value);
-      setValue(''); // value 값 초기화
-
-      // submit 이벤트는 브라우저에서 새로고침을 발생시킨다.
-      // 이를 방지하기 위해 이 함수를 호출한다.
+      if (value.trim() !== '') {
+        onInsert(value);
+        setValue(''); // value 값 초기화
+        // submit 이벤트는 브라우저에서 새로고침을 발생시킨다.
+        // 이를 방지하기 위해 이 함수를 호출한다.
+      }
       e.preventDefault();
     },
     [onInsert, value],
